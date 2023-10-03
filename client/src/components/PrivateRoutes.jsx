55f22d4a7579bb1bc668eae97545b056a0b71f07
dependentmadani <madani.badaoui12@gmail.com>
2023-10-03 19:48:37 +0100

    update: working on client side

---
+++ client/src/components/PrivateRoutes.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+import useAuth from '../hooks/useAuth';
+
+function PrivateRoutes() {
+  const { auth } = useAuth();
+
+  if (auth === undefined) {
+    return 'loading...';
+  }
+  return auth === true ? <Outlet /> : <Navigate to="/auth" />;
+}
+
+export default PrivateRoutes;
